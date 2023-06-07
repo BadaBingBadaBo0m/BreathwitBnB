12ef85879d2eace5ad174f003b7ad4b258a533ee
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-07 17:39:55 -0400

    Add an API router

---
+++ backend/routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+// router.get('/test', function(req, res) {
+//   res.json('working')
+// })
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+module.exports = router;
