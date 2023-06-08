4888cfb8b48a4549611b934832e9d251b6184618
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-08 17:49:27 -0400

    added a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -41,6 +41,22 @@ router.post('/', async (req, res, next) => {
   });
 });
 
+router.get('/', (req, res) => {
+  const { user } = req;
+
+  if (user) {
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+    
+    return res.json({
+      user: safeUser
+    });
+  } else return res.json({ user: null });
+});
+
 // Log out
 router.delete('/', async (req, res) => {
   res.clearCookie('token');
