bfe4b5634453cb33d94e8568347e1e43fbaa6973
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-11 19:39:26 -0400

    added 2 more spots to seed data

--- backend/db/seeders/20230609191857-demo-spot.js
+++ backend/db/seeders/20230609191857-demo-spot.js
@@ -24,6 +24,30 @@ module.exports = {
         name: 'App Academy',
         description: 'Place where web developers are created',
         price: 123
+      },
+      {
+        ownerId: 2,
+        address: '1176 Briarcreek Rd',
+        city: 'King',
+        state: 'North Carolina',
+        country: 'United States of America',
+        lat: 36.3259,
+        lng: -80.3641,
+        name: 'Rustic log Cabin',
+        description: 'A lovely cabin in the woods perfect for anyone wanting to get away from it all.',
+        price: 90
+      },
+      {
+        ownerId: 3,
+        address: '336 Joes Way',
+        city: 'Sparta',
+        state: 'North Carolina',
+        country: 'United States of America',
+        lat: 36.4404,
+        lng: -81.1421,
+        name: 'Rustic mansion',
+        description: "A massive cabin center in the middle of nowhere, perfect for any big family's weekend getaway",
+        price: 200
       }
     ]);
   },
@@ -32,7 +56,7 @@ module.exports = {
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return await queryInterface.bulkDelete(options, {
-      address: { [Op.in]: ['123 Disney Lane'] }
+      address: { [Op.in]: ['123 Disney Lane', '1176 Briarcreek Rd', '336 Joes Way'] }
     })
   }
 };
