16bf3272706b083e98d17b6ab6b6816a5b8d846b
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-12 16:44:20 -0400

    added route to get all Spots of current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 const { Spot, Review, SpotImage } = require('../../db/models');
 
-router.get('/', async (req, res) => {
-  const spots = await Spot.findAll();
-
+const avgRatingAndPreviewImg = async (spots) => {
   for (spot of spots) {
     const reviews = await Review.findAll({
       where: {
@@ -32,8 +30,35 @@ router.get('/', async (req, res) => {
       spot.dataValues.previewImage = spotImage.url;
     }
   }
+  
+  return spots
+}
 
-  res.json(spots);
-})
+router.get('/current', async (req, res, next) => {
+  if (!req.user) {
+    const err = new Error('Must be logged in to view');
+    err.status = 401;
+    err.title = 'Login failed';
+    return next(err);
+  }
+
+  const spots = await Spot.findAll({
+    where: {
+      ownerId: req.user.id
+    }
+  })
+
+  res.json({ Spots: await avgRatingAndPreviewImg(spots) });
+});
+
+router.get('/:id', async (req, res) => {
+
+});
+
+router.get('/', async (req, res) => {
+  const spots = await Spot.findAll();
+
+  res.json({ Spots: await avgRatingAndPreviewImg(spots) });
+});
 
 module.exports = router;
