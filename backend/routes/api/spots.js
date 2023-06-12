fe0f5df95c8a26b413b536f55689c8af5c755cf5
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-12 12:51:13 -0400

    made route to get all spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const { Spot } = require('../../db/models');
+
 router.get('/', async (req, res) => {
-  res.json('route is working')
+  const spots = await Spot.findAll();
+  res.json(spots)
 })
 
 module.exports = router;
