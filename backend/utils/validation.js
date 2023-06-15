afb6e0f65f1b3a77961343573da2a9b4db1404c7
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-15 13:13:34 -0400

    fixed bug in validations

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -21,25 +21,6 @@ const handleValidationErrors = (req, _res, next) => {
   next();
 };
 
-const handleQueryErrors = (req, _res, next) => {
-  const validationErrors = validationResult(req);
-
-  if (!validationErrors.isEmpty()) {
-    const errors = {};
-    validationErrors
-      .array()
-      .forEach(error => errors[error.path] = error.msg);
-
-    const err = Error("Bad request.");
-    err.errors = errors;
-    err.status = 400;
-    err.title = "Bad request.";
-    next(err);
-  }
-  next();
-};
-
 module.exports = {
   handleValidationErrors,
-  handleQueryErrors
 };
