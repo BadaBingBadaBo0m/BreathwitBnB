1a8c516fa9ac5a87f63984d04e2646322b540259
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-14 20:15:36 -0400

    added route to delete review images

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,8 @@ const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js');
 const reviewsRouter = require('./reviews.js');
 const bookingsRouter = require('./bookings.js');
-const reviewImageRouter = require('./spot-images.js');
+const spotImageRouter = require('./spot-images.js');
+const reviewImageRouter = require('./review-images.js');
 const { restoreUser } = require('../../utils/auth.js');
 const { requireAuth } = require('../../utils/auth.js');
 
@@ -20,7 +21,9 @@ router.use('/reviews', reviewsRouter);
 
 router.use('/bookings', bookingsRouter);
 
-router.use('/spot-images', reviewImageRouter);
+router.use('/spot-images', spotImageRouter);
+
+router.use('/review-images', reviewImageRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
