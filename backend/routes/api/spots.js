243c3d1ffb87b45ef1297a4eaabac5d172e466e7
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-15 19:10:16 -0400

    added check if its a valid date to create a booking

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -412,6 +412,33 @@ router.post('/:spotId/bookings', restoreUser, async (req, res) => {
     res.status(403);
     return res.json(err);
   }
+
+  if (newStartDate == 'Invalid Date' && newEndDate == 'Invalid Date') {
+    const err = new Error();
+    err.message = "Bad Request";
+    err.errors = {
+      startDate: "Start date must be a valid date",
+      endDate: "End date must be a valid date"
+    }
+    res.status(403);
+    return res.json(err);
+  }
+
+  if (newStartDate == 'Invalid Date') {
+    const err = new Error();
+    err.message = "Bad Request";
+    err.errors = { startDate: "Start date must be a valid date" }
+    res.status(403);
+    return res.json(err);
+  }
+
+  if (newEndDate == 'Invalid Date') {
+    const err = new Error();
+    err.message = "Bad Request";
+    err.errors = { endDate: "End date must be a valid date" }
+    res.status(403);
+    return res.json(err);
+  }
   
   // END DATE SHOULD ALWAYS BE GREATER THAN START DATE 
   if (newStartDate >= newEndDate) {
