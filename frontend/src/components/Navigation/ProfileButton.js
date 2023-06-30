2601df1c1e6be658895a35a21c6ea54c978ff133
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-30 13:30:42 -0400

    import FontAwsome and added Navigation component

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,9 @@
+const ProfileButton = () => {
+  return (
+    <div style={{ color: "red", fontSize: "50px" }}>
+      <i class="fa-solid fa-user"></i>
+    </div>
+  );
+};
+
+export default ProfileButton;
