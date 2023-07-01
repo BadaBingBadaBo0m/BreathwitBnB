248e1bf3627fea33f0cd2996a3a0c286d4acb3a7
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-06-30 22:49:52 -0400

    styled Landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { getAllSpots } from "../../store/spots";
-import { NavLink } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './LandingPage.css'
 
 
 const LandingPage = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const spotObj = useSelector((state) => state.spots.allSpots);
 
 
@@ -18,6 +19,10 @@ const LandingPage = () => {
     getSpots();
   }, [dispatch])
 
+  const handleClick = (spotId) => {
+    history.push(`/spots/${spotId}`)
+  }
+
   if (!spotObj) return null;
 
   const spotList = Object.values(spotObj)
@@ -27,11 +32,17 @@ const LandingPage = () => {
       <ul id='spotList'>
         {spotList.map(spot => (
           <li key={spot.id} className='spot'>
-            <img className='spotImage' src={spot.previewImage}></img>
-            <div>
-              <h2>{spot.name}</h2>
-              <p><i className="fa-solid fa-star"></i> {spot.avgRating}</p>
-              <p>${spot.price} night</p>
+            <img
+              className='spotImage'
+              src={spot.previewImage}
+              onClick={() => handleClick(spot.id)}>
+            </img>
+            <div id='spotInfoContainer'>
+              <div id='locationRatingContainer'>
+                <h2 onClick={() => handleClick(spot.id)}>{spot.city}, {spot.state}</h2>
+                <p><i className="fa-solid fa-star"></i> {spot.avgRating}</p>
+              </div>
+              <p id='spotPrice'>${spot.price} night</p>
             </div>
           </li>
         ))}
