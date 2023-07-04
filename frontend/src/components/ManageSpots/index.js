8f76d4fa7ca8033d64e397bdc310176a5ec41f9b
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-04 14:39:32 -0400

    added create new spot button

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -1,9 +1,12 @@
 import { useEffect } from "react";
 import { getUserSpots } from "../../store/user";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import './manageSpots.css';
 
 const ManageSpots = () => {
   const dispatch = useDispatch();
+  const spotObj = useSelector((state) => state.user.spots)
 
   useEffect(() => {
     const getSpots = () => {
@@ -13,9 +16,16 @@ const ManageSpots = () => {
     getSpots();
   }, [dispatch]);
 
+  if (Object.keys(spotObj).length <= 0) return null;
+
   return (
-    <div>
-      <h2>Working Component</h2>
+    <div id="manageSpotsContainer">
+      <h2>Manage spots</h2>
+      <NavLink to='/spots/new'> <button id="createNewSpotButton">Create a New Spot</button> </NavLink>
+
+      <div id="userSpotListContainer">
+
+      </div>
     </div>
   );
 };
