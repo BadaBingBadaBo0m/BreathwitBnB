5c1c1bcb991619f358d7def3b9b21d0bdbfb5c6c
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-04 15:31:58 -0400

    redid styling on home page so all spts are in a grid

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -7,6 +7,7 @@ import './manageSpots.css';
 const ManageSpots = () => {
   const dispatch = useDispatch();
   const spotObj = useSelector((state) => state.user.spots)
+  const spotList = Object.values(spotObj);
 
   useEffect(() => {
     const getSpots = () => {
@@ -18,14 +19,36 @@ const ManageSpots = () => {
 
   if (Object.keys(spotObj).length <= 0) return null;
 
+  const handleClick = () => {
+
+  }
+
   return (
     <div id="manageSpotsContainer">
       <h2>Manage spots</h2>
       <NavLink to='/spots/new'> <button id="createNewSpotButton">Create a New Spot</button> </NavLink>
 
       <div id="userSpotListContainer">
-
+        <ul id="spotList">
+          {spotList.map(spot => (
+            <li key={spot.id} className='spot'>
+              <img
+                className='spotImage'
+                src={spot.previewImage}
+                onClick={() => handleClick(spot.id)}>
+              </img>
+              <div id='spotInfoContainer'>
+                <div id='locationRatingContainer'>
+                  <h2 onClick={() => handleClick(spot.id)}>{spot.city}, {spot.state}</h2>
+                  <p><i className="fa-solid fa-star"></i>{spot.avgRating || "New"}</p>
+                </div>
+                <p id='spotPrice'>${spot.price} night</p>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
+
     </div>
   );
 };
