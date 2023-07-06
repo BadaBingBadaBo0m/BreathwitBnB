72c0a1db2cb790e48cd31000654454f7dedcdff9
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-05 23:56:16 -0400

    remade the backend average rating

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -24,7 +24,15 @@ const avgRatingAndPreviewImg = async (spots) => {
     // spot.dataValues.avgRating = count / stars.length;
 
     const averageRating = count / stars.length;
-    spot.dataValues.avgRating = Math.floor(averageRating * 10) / 10; // Round down to 1 decimal place
+    // spot.dataValues.avgRating = averageRating.toFixed(2);
+    if (averageRating % 1 === 0) {
+      spot.dataValues.avgRating = averageRating.toFixed(1);
+    } else {
+      spot.dataValues.avgRating = averageRating.toFixed(2);
+    }
+
+    // const averageRating = count / stars.length;
+    // spot.dataValues.avgRating = Math.floor(averageRating * 10) / 10; // Round down to 1 decimal place
 
     const spotImages = await SpotImage.findAll({
       where: {
@@ -363,8 +371,16 @@ router.get('/:spotId', async (req, res) => {
   }
 
   // spot.dataValues.avgRating = count / stars.length;
+  // const averageRating = count / stars.length;
+  // spot.dataValues.avgRating = Math.floor(averageRating * 10) / 10;
   const averageRating = count / stars.length;
-  spot.dataValues.avgRating = Math.floor(averageRating * 10) / 10;
+  // spot.dataValues.avgRating = averageRating.toFixed(2);
+  if (averageRating % 1 === 0) {
+    spot.dataValues.avgRating = averageRating.toFixed(1);
+  } else {
+    spot.dataValues.avgRating = averageRating.toFixed(2);
+  }
+
   spot.dataValues.numReviews = numReviews;
 
   res.json(spot);
