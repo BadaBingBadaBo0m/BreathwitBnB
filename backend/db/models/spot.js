22eece7564ec02ad2264a1f3a32be4b466bd03a9
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-07-06 19:39:17 -0400

    changed description data type

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -56,7 +56,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     price: {
       type: DataTypes.DECIMAL,
