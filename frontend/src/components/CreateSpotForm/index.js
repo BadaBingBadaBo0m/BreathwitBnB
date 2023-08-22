10ecb2647a1b07e0dbf8d166595475a27d1cbffb
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-23 00:17:20 +0100

    restyled create spot form a bit

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -79,7 +79,9 @@ const CreateSpotForm = () => {
 
     if (!country) errorObj.country = "Country is required";
 
-    if (!address) errorObj.address = "Address is required";
+    if (!address) errorObj.address = "is required";
+
+    if (address.length > 100) errorObj.address = "Address must be less than 100 characters"
 
     if (!city) errorObj.city = "City is required";
 
@@ -130,163 +132,170 @@ const CreateSpotForm = () => {
   return (
     <div id="formContainer">
       <form id='createSpotForm' onSubmit={handleSubmit}>
-        <div id='formHeaderContainer'>
-          <h1>Create a new Spot</h1>
-          <h2>Where's your place located?</h2>
-          <p>Guests will only get your exact address once they booked a reservation</p>
-        </div>
+        <div id='spotFormInfoContainer'>
+          <div id='formHeaderContainer'>
+            <h1>Create a new Spot</h1>
+          </div>
 
-        <div id='locationContainer' className='bottomBorder'>
-          <label className="createSpotLabel country">
-            <div>Country {errors.country && <p className='locationErrors'>{errors.country}</p>}</div>
-            <select
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-            >
-              {countryList.map(country => (
-                <option key={country} value={country}>{country}</option>
-              ))}
-            </select>
-          </label>
-          <label className="createSpotLabel streetAdd">
-            <div>Street Address {errors.address && <p className='locationErrors'>{errors.address}</p>}</div>
-            <input
-              type='text'
-              placeholder='Address'
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </label>
-          <div id='cityStateContainer'>
-            <label className="createSpotLabel city">
-              <div>City {errors.city && <p className='locationErrors'>{errors.city}</p>}</div>
-              <input
-                type='text'
-                placeholder='City'
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
+          <div id='locationContainer' className='bottomBorder'>
+            <h2>Where's your place located?</h2>
+            <p>Guests will only get your exact address once they booked a reservation</p>
+            <div id='country-streetAdd-container'>
+              <label className="createSpotLabel country">
+                <div>Country {errors.country && <p className='locationErrors'>{errors.country}</p>}</div>
+                <select
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                >
+                  {countryList.map(country => (
+                    <option key={country} value={country}>{country}</option>
+                  ))}
+                </select>
+              </label>
+
+              <label className="createSpotLabel streetAdd">
+                <p className='streetAddLabel'>Street Address {errors.address && <span className='locationErrors'>{errors.address}</span>}</p>
+                <input
+                  type='text'
+                  placeholder='Address'
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <div id='cityStateContainer'>
+              <label className="createSpotLabel city">
+                <p className='streetAddLabel'>City {errors.city && <span className='locationErrors'>{errors.city}</span>}</p>
+                <input
+                  type='text'
+                  placeholder='City'
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+              </label >
+              <p id='cityStateComma'>,</p>
+              <label className="createSpotLabel state">
+                <div>State {errors.state && <p className='locationErrors'>{errors.state}</p>}</div>
+                <select
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                >
+                  {stateList.map(state => (
+                    <option key={state} value={state}>{state}</option>
+                  ))}
+                </select>
+              </label>
+            </div>
+          </div>
+
+          <div id='createSpotInfoContainer'>
+            <div id='createSpotDescription' className='bottomBorder'>
+              <h2 className='createSpotFormH2'>Describe your place to guests</h2>
+              <p className='createSpotFormP'>
+                Mention the best features of your space, any special amenities like fast wifi or parking,
+                and what you love about the neighborhood.
+              </p>
+              <textarea
+                id='descriptionInput'
+                placeholder='Please write at least 30 characters'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
-            </label >
-            <p id='cityStateComma'>,</p>
-            <label className="createSpotLabel state">
-              <div>State {errors.state && <p className='locationErrors'>{errors.state}</p>}</div>
-              <select
-                value={state}
-                onChange={(e) => setState(e.target.value)}
-              >
-                {stateList.map(state => (
-                  <option key={state} value={state}>{state}</option>
-                ))}
-              </select>
-            </label>
-          </div>
-        </div>
+              {errors.description && <p className='infoErrors'>{errors.description}</p>}
+            </div>
 
-        <div id='createSpotInfoContainer'>
-          <div id='createSpotDescription' className='bottomBorder'>
-            <h2 className='createSpotFormH2'>Describe your place to guests</h2>
-            <p className='createSpotFormP'>
-              Mention the best features of your space, any special amenities like fast wifi or parking,
-              and what you love about the neighborhood.
-            </p>
-            <textarea
-              id='descriptionInput'
-              placeholder='Please write at least 30 characters'
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            {errors.description && <p className='infoErrors'>{errors.description}</p>}
+            <div id='createSpotTitleContainer' className='bottomBorder'>
+              <h2 className='createSpotFormH2'>Create a title for your spot</h2>
+              <p className='createSpotFormP'>Catch guests' attention with a spot title that highlights what makes your place special</p>
+              <input
+                id='createSpotTitleInput'
+                placeholder='Name of your spot'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              {errors.title && <p className='infoErrors'>{errors.title}</p>}
+            </div>
           </div>
 
-          <div id='createSpotTitleContainer' className='bottomBorder'>
-            <h2 className='createSpotFormH2'>Create a title for your spot</h2>
-            <p className='createSpotFormP'>Catch guests' attention with a spot title that highlights what makes your place special</p>
-            <input
-              id='createSpotTitleInput'
-              placeholder='Name of your spot'
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            {errors.title && <p className='infoErrors'>{errors.title}</p>}
-          </div>
-        </div>
+          <div id='createSpotPricing' className='bottomBorder'>
+            <h2 className='createSpotFormH2'>Set a base price for your spot</h2>
+            <p className='createSpotFormP'>Competitive pricing can help your listing stand out and rank higher in search results</p>
+            <div id='dollarSignInput'>
+              <i className="fa-solid fa-dollar-sign"></i>
+              <input
+                type='number'
+                id='createSpotPriceInput'
+                placeholder='Price per night (USD)'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
 
-        <div id='createSpotPricing' className='bottomBorder'>
-          <h2 className='createSpotFormH2'>Set a base price for your spot</h2>
-          <p className='createSpotFormP'>Competitive pricing can help your listing stand out and rank higher in search results</p>
-          <div id='dollarSignInput'>
-            <i className="fa-solid fa-dollar-sign"></i>
-            <input
-              type='number'
-              id='createSpotPriceInput'
-              placeholder='Price per night (USD)'
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
+            </div>
+            {errors.price && <p className='infoErrors'>{errors.price}</p>}
           </div>
-          {errors.price && <p className='infoErrors'>{errors.price}</p>}
-        </div>
 
-        <div id='createSpotImages' className='bottomBorder'>
-          <h2 className='createSpotFormH2'>Liven up your spot with photos</h2>
-          <p className='createSpotFormP'>Submit a link to at least one photo to publish your spot</p>
-          <div className='createSpotImageContainer'>
-            <input
-              className='createFormSpotImage'
-              placeholder='Preview Image URL'
-              value={previewImage}
-              onChange={(e) => setPreviewImage(e.target.value)}
-            />
-            {errors.previewImage && <p className='infoErrors'>{errors.previewImage}</p>}
-            {errors.spotImages && errors.spotImages.imageLink1 && <p className='infoErrors'>{errors.spotImages.imageLink1}</p>}
+          <div id='createSpotImages' className='bottomBorder'>
+            <h2 className='createSpotFormH2'>Liven up your spot with photos</h2>
+            <p className='createSpotFormP'>Submit a link to at least one photo to publish your spot</p>
+            <div className='createSpotImageContainer'>
+              <input
+                className='createFormSpotImage'
+                placeholder='Preview Image URL'
+                value={previewImage}
+                onChange={(e) => setPreviewImage(e.target.value)}
+              />
+              {errors.previewImage && <p className='infoErrors'>{errors.previewImage}</p>}
+              {errors.spotImages && errors.spotImages.imageLink1 && <p className='infoErrors'>{errors.spotImages.imageLink1}</p>}
+            </div>
+            <div className='createSpotImageContainer'>
+              <input
+                className='createFormSpotImage'
+                placeholder='Image URL'
+                value={spotImage2}
+                onChange={(e) => setSpotImage2(e.target.value)}
+              />
+              {errors.spotImages && errors.spotImages.imageLink2 && <p className='infoErrors'>{errors.spotImages.imageLink2}</p>}
+            </div>
+            <div className='createSpotImageContainer'>
+              <input
+                className='createFormSpotImage'
+                placeholder='Image URL'
+                value={spotImage3}
+                onChange={(e) => setSpotImage3(e.target.value)}
+              />
+              {errors.spotImages && errors.spotImages.imageLink3 && <p className='infoErrors'>{errors.spotImages.imageLink3}</p>}
+            </div>
+            <div className='createSpotImageContainer'>
+              <input
+                className='createFormSpotImage'
+                placeholder='Image URL'
+                value={spotImage4}
+                onChange={(e) => setSpotImage4(e.target.value)}
+              />
+              {errors.spotImages && errors.spotImages.imageLink4 && <p className='infoErrors'>{errors.spotImages.imageLink4}</p>}
+            </div>
+            <div className='createSpotImageContainer'>
+              <input
+                className='createFormSpotImage'
+                placeholder='Image URL'
+                value={spotImage5}
+                onChange={(e) => setSpotImage5(e.target.value)}
+              />
+              {errors.spotImages && errors.spotImages.imageLink5 && <p className='infoErrors'>{errors.spotImages.imageLink5}</p>}
+            </div>
           </div>
-          <div className='createSpotImageContainer'>
-            <input
-              className='createFormSpotImage'
-              placeholder='Image URL'
-              value={spotImage2}
-              onChange={(e) => setSpotImage2(e.target.value)}
-            />
-            {errors.spotImages && errors.spotImages.imageLink2 && <p className='infoErrors'>{errors.spotImages.imageLink2}</p>}
-          </div>
-          <div className='createSpotImageContainer'>
-            <input
-              className='createFormSpotImage'
-              placeholder='Image URL'
-              value={spotImage3}
-              onChange={(e) => setSpotImage3(e.target.value)}
-            />
-            {errors.spotImages && errors.spotImages.imageLink3 && <p className='infoErrors'>{errors.spotImages.imageLink3}</p>}
-          </div>
-          <div className='createSpotImageContainer'>
-            <input
-              className='createFormSpotImage'
-              placeholder='Image URL'
-              value={spotImage4}
-              onChange={(e) => setSpotImage4(e.target.value)}
-            />
-            {errors.spotImages && errors.spotImages.imageLink4 && <p className='infoErrors'>{errors.spotImages.imageLink4}</p>}
-          </div>
-          <div className='createSpotImageContainer'>
-            <input
-              className='createFormSpotImage'
-              placeholder='Image URL'
-              value={spotImage5}
-              onChange={(e) => setSpotImage5(e.target.value)}
-            />
-            {errors.spotImages && errors.spotImages.imageLink5 && <p className='infoErrors'>{errors.spotImages.imageLink5}</p>}
-          </div>
-        </div>
 
-        <div id='submitButtonContainer'>
-          <button
-            type='submit'
-            id='createSpotSubmit'
-          >Create Spot
-          </button>
+          <div id='submitButtonContainer'>
+            <button
+              type='submit'
+              id='createSpotSubmit'
+            >Create Spot
+            </button>
+          </div>
         </div>
-      </form>
-    </div>
+      </form >
+    </div >
   )
 }
 
