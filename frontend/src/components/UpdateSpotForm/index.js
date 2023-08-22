5783d078f466ffc7eec96a5d5cab4b08bf3090b8
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-08-23 00:24:41 +0100

    restyled create and update forms a bit

--- frontend/src/components/UpdateSpotForm/index.js
+++ frontend/src/components/UpdateSpotForm/index.js
@@ -111,109 +111,116 @@ const UpdateSpotForm = () => {
   return (
     <div id="formContainer">
       <form id='createSpotForm' onSubmit={handleSubmit}>
-        <div id='formHeaderContainer'>
-          <h1>Update your Spot</h1>
-          <h2>Where's your place located?</h2>
-          <p>Guests will only get your exact address once they booked a reservation</p>
-        </div>
+        <div id='spotFormInfoContainer'>
+          <div id='formHeaderContainer'>
+            <h1>Update Spot</h1>
+          </div>
 
-        <div id='locationContainer' className='bottomBorder'>
-          <label className="createSpotLabel country">
-            <div>Country {errors.country && <p className='locationErrors'>{errors.country}</p>}</div>
-            <select
-              value={country}
-              onChange={(e) => setCountry(e.target.value)}
-            >
-              {countryList.map(country => (
-                <option key={country} value={country}>{country}</option>
-              ))}
-            </select>
-          </label>
-          <label className="createSpotLabel streetAdd">
-            <div>Street Address {errors.address && <p className='locationErrors'>{errors.address}</p>}</div>
-            <input
-              type='text'
-              placeholder='Address'
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            />
-          </label>
-          <div id='cityStateContainer'>
-            <label className="createSpotLabel city">
-              <div>City {errors.city && <p className='locationErrors'>{errors.city}</p>}</div>
-              <input
-                type='text'
-                placeholder='City'
-                value={city}
-                onChange={(e) => setCity(e.target.value)}
+          <div id='locationContainer' className='bottomBorder'>
+            <h2>Where's your place located?</h2>
+            <p>Guests will only get your exact address once they booked a reservation</p>
+            <div id='country-streetAdd-container'>
+              <label className="createSpotLabel country">
+                <div>Country {errors.country && <p className='locationErrors'>{errors.country}</p>}</div>
+                <select
+                  value={country}
+                  onChange={(e) => setCountry(e.target.value)}
+                >
+                  {countryList.map(country => (
+                    <option key={country} value={country}>{country}</option>
+                  ))}
+                </select>
+              </label>
+
+              <label className="createSpotLabel streetAdd">
+                <p className='streetAddLabel'>Street Address {errors.address && <span className='locationErrors'>{errors.address}</span>}</p>
+                <input
+                  type='text'
+                  placeholder='Address'
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </label>
+            </div>
+
+            <div id='cityStateContainer'>
+              <label className="createSpotLabel city">
+                <p className='streetAddLabel'>City {errors.city && <span className='locationErrors'>{errors.city}</span>}</p>
+                <input
+                  type='text'
+                  placeholder='City'
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+              </label >
+              <p id='cityStateComma'>,</p>
+              <label className="createSpotLabel state">
+                <div>State {errors.state && <p className='locationErrors'>{errors.state}</p>}</div>
+                <select
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                >
+                  {stateList.map(state => (
+                    <option key={state} value={state}>{state}</option>
+                  ))}
+                </select>
+              </label>
+            </div>
+          </div>
+
+          <div id='createSpotInfoContainer'>
+            <div id='createSpotDescription' className='bottomBorder'>
+              <h2 className='createSpotFormH2'>Describe your place to guests</h2>
+              <p className='createSpotFormP'>
+                Mention the best features of your space, any special amenities like fast wifi or parking,
+                and what you love about the neighborhood.
+              </p>
+              <textarea
+                id='descriptionInput'
+                placeholder='Please write at least 30 characters'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
               />
-            </label >
-            <p id='cityStateComma'>,</p>
-            <label className="createSpotLabel state">
-              <div>State {errors.state && <p className='locationErrors'>{errors.state}</p>}</div>
-              <select
-                value={state}
-                onChange={(e) => setState(e.target.value)}
-              >
-                {stateList.map(state => (
-                  <option key={state} value={state}>{state}</option>
-                ))}
-              </select>
-            </label>
-          </div>
-        </div>
+              {errors.description && <p className='infoErrors'>{errors.description}</p>}
+            </div>
 
-        <div id='createSpotInfoContainer'>
-          <div id='createSpotDescription' className='bottomBorder'>
-            <h2 className='createSpotFormH2'>Describe your place to guests</h2>
-            <p className='createSpotFormP'>
-              Mention the best features of your space, any special amenities like fast wifi or parking,
-              and what you love about the neighborhood.
-            </p>
-            <textarea
-              id='descriptionInput'
-              placeholder='Please write at least 30 characters'
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-            {errors.description && <p className='infoErrors'>{errors.description}</p>}
+            <div id='createSpotTitleContainer' className='bottomBorder'>
+              <h2 className='createSpotFormH2'>Update the title for your spot</h2>
+              <p className='createSpotFormP'>Catch guests' attention with a spot title that highlights what makes your place special</p>
+              <input
+                id='createSpotTitleInput'
+                placeholder='Name of your spot'
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              {errors.title && <p className='infoErrors'>{errors.title}</p>}
+            </div>
           </div>
 
-          <div id='createSpotTitleContainer' className='bottomBorder'>
-            <h2 className='createSpotFormH2'>Create a title for your spot</h2>
-            <p className='createSpotFormP'>Catch guests' attention with a spot title that highlights what makes your place special</p>
-            <input
-              id='createSpotTitleInput'
-              placeholder='Name of your spot'
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-            {errors.title && <p className='infoErrors'>{errors.title}</p>}
-          </div>
-        </div>
+          <div id='createSpotPricing' className='bottomBorder'>
+            <h2 className='createSpotFormH2'>Update the price for your spot</h2>
+            <p className='createSpotFormP'>Competitive pricing can help your listing stand out and rank higher in search results</p>
+            <div id='dollarSignInput'>
+              <i className="fa-solid fa-dollar-sign"></i>
+              <input
+                type='number'
+                id='createSpotPriceInput'
+                placeholder='Price per night (USD)'
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              />
 
-        <div id='createSpotPricing' className='bottomBorder'>
-          <h2 className='createSpotFormH2'>Set a base price for your spot</h2>
-          <p className='createSpotFormP'>Competitive pricing can help your listing stand out and rank higher in search results</p>
-          <div id='dollarSignInput'>
-            <i className="fa-solid fa-dollar-sign"></i>
-            <input
-              type='number'
-              id='createSpotPriceInput'
-              placeholder='Price per night (USD)'
-              value={price}
-              onChange={(e) => setPrice(e.target.value)}
-            />
+            </div>
+            {errors.price && <p className='infoErrors'>{errors.price}</p>}
           </div>
-          {errors.price && <p className='infoErrors'>{errors.price}</p>}
-        </div>
 
-        <div id='submitButtonContainer'>
-          <button
-            type='submit'
-            id='createSpotSubmit'
-          >Update Spot
-          </button>
+          <div id='submitButtonContainer'>
+            <button
+              type='submit'
+              id='createSpotSubmit'
+            >Update Spot
+            </button>
+          </div>
         </div>
       </form>
     </div>
