5d20aa62db184ed7b8945fc8be5b6046a14890f7
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2023-09-06 12:12:54 -0400

    added in datepicker and is semi-functional but leaving for now to prioritize capstone project

--- frontend/src/components/SpotDetails/PriceReserveReviews.js
+++ frontend/src/components/SpotDetails/PriceReserveReviews.js
@@ -1,6 +1,23 @@
+import { forwardRef, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import './PriceReserveReviews.css'
 
 const PriceReserveReviews = ({ spot }) => {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const ReserveButton = forwardRef(({ value, onClick }, ref) => (
+    <button id="reserveButton" className="example-custom-input" onClick={onClick} ref={ref}>
+      {startDate && endDate ? value : "Reserve"}
+    </button>
+  ));
+
+  const onChange = (dates) => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
 
   const handleClick = () => {
     alert('Feature Coming Soon...')
@@ -20,6 +37,21 @@ const PriceReserveReviews = ({ spot }) => {
         </div>
       </div>
       <button onClick={handleClick} id="reserveButton">Reserve</button>
+      {/* <DatePicker
+        // customInput={<ReserveButton />}
+        customInput={<ReserveButton />}
+
+        selectsRange={true}
+        startDate={startDate}
+        endDate={endDate}
+        onChange={onChange}
+        isClearable={true}
+        shouldCloseOnSelect={false}
+
+      // excludeDateIntervals={[
+      //   // { start: subDays(new Date(), 5), end: addDays(new Date(), 5) },
+      // ]}
+      /> */}
     </div>
   );
 };
