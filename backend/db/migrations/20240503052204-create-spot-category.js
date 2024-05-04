b7afa502696f8ec49c49eabc3313cca399248206
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-05-03 19:38:58 -0700

    trying spot instead of spots

--- backend/db/migrations/20240503052204-create-spot-category.js
+++ backend/db/migrations/20240503052204-create-spot-category.js
@@ -21,7 +21,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'Spots',
+          model: 'Spot',
           key: 'id'
         },
       },
