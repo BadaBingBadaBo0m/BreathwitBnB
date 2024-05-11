fb71c0684de0c370a0de7e9d44aee7e7f935a2ac
BadaBingBadaBo0m <alexbreathwitone@gmail.com>
2024-05-11 02:54:26 -0700

    added arrow buttons to scroll categories left and right

--- frontend/src/components/Navigation/Categories.js
+++ frontend/src/components/Navigation/Categories.js
@@ -1,10 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkGetCategories } from "../../store/categories";
 
 const Categories = () => {
   const dispatch = useDispatch()
   const categoryArr = useSelector((state) => state.categories?.categoryList);
+  const categoryScrollRef = useRef(null);
+  const [showLeftButton, setShowLeftButton] = useState(false);
+  const [showRightButton, setShowRightButton] = useState(false);
 
   useEffect(() => {
     const getCategories = async () => {
@@ -14,12 +17,43 @@ const Categories = () => {
     getCategories();
   }, [])
 
-  if (!categoryArr || categoryArr.length === 0) return (<h2>Loading</h2>)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (categoryScrollRef.current) {
+        setShowLeftButton(categoryScrollRef.current.scrollLeft > 0);
+        setShowRightButton(categoryScrollRef.current.scrollLeft < categoryScrollRef.current.scrollWidth - window.innerWidth);
+      }
+    };
 
+    const updateScrollButtons = () => {
+      handleScroll();
+    };
+
+    if (categoryScrollRef.current) {
+      categoryScrollRef.current.addEventListener('scroll', handleScroll);
+    }
+
+    updateScrollButtons();
+
+    return () => {
+      if (categoryScrollRef.current) {
+        categoryScrollRef.current.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [categoryArr]);
+
+  if (!categoryArr || categoryArr.length === 0) return (<h2>Loading</h2>);
+
+  const scrollRight = () => {
+    categoryScrollRef.current.scrollLeft += 200;
+  }
+
+  const scrollLeft = () => {
+    categoryScrollRef.current.scrollLeft -= 200;
+  }
 
   return (
-    <ul id="categoryListContainer">
-      <button className="scrollButton left">arrow</button>
+    <ul id="categoryListContainer" ref={categoryScrollRef}>
       {categoryArr.map(category => (
         <>
           <li
@@ -31,7 +65,8 @@ const Categories = () => {
           </li>
         </>
       ))}
-      <button className="scrollButton right">arrow</button>
+      {showRightButton && <button className="scrollButton right" onClick={scrollRight}>arrow</button>}
+      {showLeftButton && <button className="scrollButton left" onClick={scrollLeft}>arrow</button>}
     </ul>
   );
 };
